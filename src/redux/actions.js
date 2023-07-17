d8b3a54f1e3bb002ae2be53542b815049f4ed08e
Anastasiia <dessertkiev@gmail.com>
2023-07-17 19:16:27 +0300

    .

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,29 +1,14 @@
-import { nanoid } from "nanoid";
-export const addTask = text => {
+import { createAction, nanoid } from "@reduxjs/toolkit";
+
+export const addTask = createAction("tasks/addTask", (text) => {
   return {
-    type: "tasks/addTask",
     payload: {
+      text,
       id: nanoid(),
       completed: false,
-      text,
     },
   };
-};
-export const deleteTask = taskId => {
-  return {
-    type: "tasks/deleteTask",
-    payload: taskId,
-  };
-};
-export const toggleCompleted = taskId => {
-  return {
-    type: "tasks/toggleCompleted",
-    payload: taskId,
-  };
-};
-export const setStatusFilter = value => {
-  return {
-    type: "filters/setStatusFilter",
-    payload: value,
-  };
-};
+});
+export const deleteTask = createAction("tasks/deleteTask");
+export const toggleCompleted = createAction("tasks/toggleCompleted");
+export const setStatusFilter = createAction("filters/setStatusFilter");
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from "./reducer";
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+import { configureStore } from "@reduxjs/toolkit";
+import { tasksReducer, filtersReducer } from "./reducer";
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
